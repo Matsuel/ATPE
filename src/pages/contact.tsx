dc43eff652763d41cf84f77228a25cd07f9c24d1
Matsuel <matsuel0308@gmail.com>
2024-03-06 18:13:25 +0100

    create contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from '@/styles/Contact.module.scss';
+
+interface ContactProps {
+
+}
+
+const Contact = ({ }: ContactProps) => {
+    return (
+        <div className={styles.Contact_container}>
+            
+        </div>
+    );
+};
+
+export default Contact;
