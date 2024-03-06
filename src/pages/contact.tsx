bc8b290600173236745fe083a2c86e242ba73d33
Matsuel <matsuel0308@gmail.com>
2024-03-06 18:42:29 +0100

    patch form bug with position absolute

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,11 +13,27 @@ const Contact = ({ }: ContactProps) => {
         <div className={styles.Contact_container}>
             <NavBar />
 
-            <div  className={styles.ContactImage}>
+            <div className={styles.ContactImage}>
                 <h1 className={styles.ContactTitle}>Rencontrons-nous</h1>
             </div>
-            
-            
+
+
+
+            <div className={styles.ContactContent}>
+                <h2 className={styles.ContactSubTitle}>Contactez-nous</h2>
+                <p className={styles.ContactText}>En remplissant le formulaire ci-dessous.</p>
+                <div className={styles.ContactForm}>
+                    <input type="text" placeholder="Nom" />
+                    <input type="text" placeholder="Prénom" />
+                    <input type="number" placeholder="Téléphone" maxLength={10} />
+                    <input type="text" placeholder="Email" maxLength={150} />
+                    <textarea placeholder="Votre message"></textarea>
+                    <button>Envoyer</button>
+                </div>
+            </div>
+
+
+
         </div>
     );
 };
