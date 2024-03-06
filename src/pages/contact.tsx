c8c080cf45f91033f69c4b1256bda8c1589df190
Matsuel <matsuel0308@gmail.com>
2024-03-06 18:31:19 +0100

    contact title + background

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import NavBar from '@/Components/Navbar';
 import styles from '@/styles/Contact.module.scss';
+import Image from 'next/image';
+import Img3 from '@/assets/imgEscrime3.png';
 
 interface ContactProps {
 
@@ -9,6 +11,12 @@ interface ContactProps {
 const Contact = ({ }: ContactProps) => {
     return (
         <div className={styles.Contact_container}>
+            <NavBar />
+
+            <div  className={styles.ContactImage}>
+                <h1 className={styles.ContactTitle}>Rencontrons-nous</h1>
+            </div>
+            
             
         </div>
     );
