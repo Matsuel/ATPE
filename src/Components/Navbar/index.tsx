0bab61783ac74c06507985fd37736ba91afa9b75
Matsuel <matsuel0308@gmail.com>
2024-03-06 10:17:13 +0100

    init repo and navbar component

---
+++ src/Components/Navbar/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './style.module.scss';
+import Logo from '@/assets/logo.png'
+
+interface NavBarProps {
+
+}
+
+interface Tab {
+    name: string;
+    link: string;
+}
+
+const NavBar = ({ }: NavBarProps) => {
+    const tab: Tab[] = [
+        {
+            name: "Acceuil",
+            link: "/"
+        },
+        {
+            name: "Particuliers",
+            link: "/particuliers"
+        },
+        {
+            name: "Professionnels",
+            link: "/professionnels"
+        },
+        {
+            name: "Contact",
+            link: "/contact"
+        },
+        {
+            name: "A propos",
+            link: "/a-propos"
+        },
+    ]
+
+    return (
+        <div className={styles.NavBar_container}>
+            <div className={styles.NavBar_Content}>
+                <Image src={Logo} alt="logo" width={200} />
+                {
+                    tab.map((item: Tab, index: number) => {
+                        return (
+                            <a key={index} href={item.link} className={styles.Navbar_tab}>{item.name}</a>
+                        )
+                    })
+                }
+
+            </div>
+
+
+
+        </div>
+    );
+};
+
+export default NavBar;
