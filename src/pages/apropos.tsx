294eaabfda22e05b487f240b3564a31388da3bf4
Matsuel <matsuel0308@gmail.com>
2024-03-07 11:21:01 +0100

    apropos page

--- src/pages/apropos.tsx
+++ src/pages/apropos.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import styles from '@/styles/APropos.module.scss';
 import NavBar from '@/Components/Navbar';
 import Membres from '@/datas/members';
+import Footer from '@/Components/Footer';
 
 interface AProposProps {
 
@@ -43,6 +44,22 @@ const APropos = ({ }: AProposProps) => {
                     })
                 }                
             </div>
+
+            <div className={styles.AProposReseau}>
+                <h2 className={styles.AProposSubTitle}>Notre réseau</h2>
+                <p className={styles.AProposText}>Notre équipe s'appuie sur un médecin référent, un médecin du sport spécialisé en traumatologie, des psychologues du travail, des psychologues cliniciens, des thérapeutes psychocorporels ainsi qu'un psychiatre.</p>
+
+                <div className={styles.AProposReseauImages}>
+                    <Image src={require('@/assets/imgEscrime6.jpg')} alt="Escrime" className={styles.AProposReseauImage} />
+                    <Image src={require('@/assets/imgEscrime5.jpg')} alt="Escrime" className={styles.AProposReseauImage} />
+                </div>
+
+                <a className={styles.AProposReseauButton} href="/contact">
+                    Rejoindre notre réseau
+                </a>
+            </div>
+
+            <Footer />
         </div>
     );
 };
