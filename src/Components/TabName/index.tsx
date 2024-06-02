579f4efd8a80ab2af7e1711d592e6709f549d67a
Matsuel <matsuel0308@gmail.com>
2024-06-02 12:31:42 +0200

    add favicon

--- src/Components/TabName/index.tsx
+++ src/Components/TabName/index.tsx
@@ -11,6 +11,8 @@ const TabName = ({name}: TabNameProps) => {
     return (
         <Head>
             <title>{name}</title>
+            <meta name="description" content="ATPEscrime" />
+            <link rel="icon" href="/logo.png" />
         </Head>
     );
 };
